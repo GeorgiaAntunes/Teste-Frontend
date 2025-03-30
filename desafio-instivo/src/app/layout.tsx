39d311d0f171f45e9bf2466b312d8e4d8a2872c8
georgia.antunes <georgiaantunes@outlook.com>
2025-03-30 18:47:18 -0300

    fix metadata

--- desafio-instivo/src/app/layout.tsx
+++ desafio-instivo/src/app/layout.tsx
@@ -4,15 +4,16 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { CssBaseline, ThemeProvider } from "@mui/material"; 
 import { theme } from "./theme/theme";
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export default function RootLayout({
--- desafio-instivo/src/app/metadata.ts
+++ desafio-instivo/src/app/metadata.ts
@@ -1,6 +1,9 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Teste Instivo",
   description: "Teste frontend para Instivo",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=5",
+  robots: "index, follow",
+  themeColor: "#ffffff",
 };
